55abaa4379963b4f78029ee4fa30d5c8a15ea59c
root <root@NTL7D>
2024-02-15 22:36:02 +0700

    a

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -41,7 +41,7 @@ export const ServerChannel = (props: ServerChannelProps) => {
       onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-        params.channelId === props.channel.id &&
+        params?.channelId === props.channel.id &&
           "bg-zinc-700/20 dark:bg-zinc-700"
       )}
     >
